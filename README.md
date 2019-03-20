889f50a44ec5f962670597c1082d8ff056e97331
Kyle Squizzato <kyle.squizzato@docker.com>
2019-03-20 15:43:39 -0700

    First version of tool
    Signed-off-by: Kyle Squizzato <kyle.squizzato@docker.com>

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # gh-mass-commentor
-Simple tool to associate old GitHub `docker/escalation` issues with those in Jira.
+Simple tool to associate old GitHub issues with those in Jira.
+
+## Usage
+1. Build the patched `go-jira` tool to facilitate the interaction with Jira
+to determine remote links:
+
+```
+go get github.com/squizzi/go-jira
+cd $GOPATH/src/squizzi/go-jira
+make install
+cp ~/bin/jira ./jira
+```
+
+2. Edit the global `gh_token` variable with a `repo` scoped GitHub authtoken.
+
+```python
+"""
+Login stuff
+"""
+global gh_token, jira_token, g
+gh_token = "YOUR FANTASTIC TOKEN HERE"
+g = Github(gh_token)
+```
+
+3. Install the requirements file:
+
+```
+pip3 install -r requirements.txt
+```
+
+4. Run it:
+
+```
+python3 ./ghcommentor.py -g docker/escalation -j FIELD -c 1200
+```
+
+Where `-g` is the Github repo, `-j` is the Jira repo and `-c` is the number of
+Jira issues in the given Jira repo.
+
+5. **Enjoy using this exceptionally hacky tool!!!**
--- ghcommentor.py
+++ ghcommentor.py
@@ -1,145 +1,70 @@
 #!/usr/bin/env python
 import sys
 import logging
-import validators
 import argparse
-import signal
-from requests import session
-from bs4 import BeautifulSoup as bs
+import itertools
+from shell import shell
+from github import Github
 from logrusformatter import LogrusFormatter
 
-"""
-ghLogin logs in to Github to form an established session for use, it also closes
-sessions
-"""
-class ghLogin():
-    def __init__(self):
-        self.s = session()
-
-    def login(self, user, password, authcode,
-              session_url='https://github.com/session',
-              two_factor_url='https://github.com/sessions/two-factor'):
-    req = self.s.get(session_url).text
-    html = bs(req, "lxml")
-    token = html.find("input", {"name": "authenticity_token"}).attrs['value']
-    com_val = html.find("input", {"name": "commit"}).attrs['value']
-    login_data = {'login' : user,
-                  'password' : password,
-                  'commit' : com_val,
-                  'authenticity_token' : token}
-    r = self.s.post(session_url data=login_data, allow_redirects=True)
-    if r.url in two_factor_url:
-        # If we're sent to the two-factor auth session page send more login
-        # data containing authcode and a new token
-        html = bs(r.text, "lxml")
-        token = html.find("input", {"name": "authenticity_token"}).attrs['value']
-        otp_data = {'otp' : authcode,
-                    'authenticity_token' : token}
-        r2 = self.s.post(r.url, data=otp_data)
-        return r2
-    else:
-        # Else we're already logged in, and have established a valid session
-        return r
-
-    def logout(self):
-        r = self.s.get('https://github.com/logout')
-        self.s.close()
-        return r
-
-""" load_config loads config variables """
-def load_config(filename):
-    configs = {}
-    # Open config file and read in values (if it exists)
-    if os.path.exists(filename):
-        config = open(filename, "r")
-        content = config.read()
-        lines = content.split("\n")
-        for data in lines:
-            # If the line returned from the config file is
-            # trying to set a value, load it up and set it in
-            # our configs dictionary
-            if data.find("=") != -1:
-                option = data.split("=")[0]
-                value = data.split("=")[1]
-                # if the value has a comma in it, we need
-                # to build an array instead of a string
-                if value.find(",") != -1:
-                    value = value.split(",")
-                configs[option] = value
-    else:
-        return load_config_from_env()
-    return configs
 
 """
-generate_config creates a config of data that stays intact between runs of
-ghgrab
+Login stuff
 """
-def generate_config():
-    # Generate a hidden file in pwd
-    config_file = ".ghauth.cfg"
-    if not os.path.isfile(config_file):
-        logging.info("No configuration file found, creating one now...")
-    else:
-        if not ("USER" in config and
-        "PASSWORD" in config and
-        "TOKEN" in config):
-            logging.info("Detected that some configuration variables may not exist in the {0} file, creating them now...").format(config_file)
-            pass
-    if not "USER" in config:
-        while True:
-            USER = raw_input("Enter GitHub username: ")
-            if not validators.slug(USER):
-                print("Please enter a valid GitHub username.")
-            else:
-                break
-        save_config(config_file, "USER",USER)
-    if not "PASSWORD" in config:
-        while True:
-            plain_password = getpass.getpass('Enter GitHub password: ')
-            print("Encrypting password, please wait...\n")
-            encrypted_password = encrypt('password', plain_password)
-            # Write the encrypted_password out to it's own file
-            open(".ghpassword".format(config_dir), "w").write(encrypted_password);
-            os.chmod(".ghpassword", 0666)
-            # Save the config so we know password has been captured
-            save_config(config_file, "PASSWORD", "ENCRYPTED")
-    if not "TOKEN" in config:
-        while True:
-            TOKEN = raw_input("Enter GitHub Personal access token: ")
-            if not validators.length(from_number, min=41):
-                print("Please enter a valid GitHub token.")
-            else:
-                break
-        save_config(config_file, "TOKEN",TOKEN)
+global gh_token, jira_token, g
+gh_token = "YOUR AWESOME TOKEN HERE"
+g = Github(gh_token)
 
 
 """
-read_issue reads a given issue_number and returns a list of attachment urls
-for downloading
+generateComments generates comments for all issues within the given gh_repository.
+The comment will feature the jira_issue that has linked the github issue.
 """
-def read_issue(issue_number):
-
-
-"""
-download_attachments downloads a given list of attachments and places them in
-a given directory structure, usually one associated with a given issue number
-"""
-def download_attachments(attachment, issue_number):
-
+def generateComments(gh_repository, jira_repository, max_range):
+    logging.info("Beginning comment generation for GH repo: {0} as associated by Jira repo: {1}".format(gh_repository, jira_repository))
+    repo = g.get_repo(gh_repository)
+    for jira_issue in range(1, max_range):
+        # Get the Github issue associated with the jira_issue
+        gh_issue_number = shell('./jira view {0}-{1}'.format(jira_repository, jira_issue)).output()
+        try:
+            # If there's no github issue associated continue to the next
+            # jira_issue
+            len(gh_issue_number[0])
+        except IndexError:
+            continue
+        logging.info("Generating comment in GH issue: {0} to associate it with {1}-{2}".format(gh_issue_number[0], jira_repository, jira_issue))
+        issue = repo.get_issue(int(gh_issue_number[0]))
+        try:
+            issue.create_comment("This issue has moved to Jira.  The new issue can be found here: https://docker.atlassian.net/browse/{0}-{1}.".format(jira_repository, jira_issue))
+        except github.GithubException.GithubException as e:
+            logging.error("Unable to add comment to GH issue: {0}".format(e))
+            continue
 
 """
 Main
 """
 def main():
     # Argument parsing
-    parser = argparse.ArgumentParser(description='Download files from GitHub \
-    issues.')
-    parser.add_argument("-a",
-                        "--authcode",
-                        dest="authcode",
-                        help="Enter your two-factor auth code.  Required for \
-                        use if two-factor authentication is enabled across \
-                        GitHub.")
+    parser = argparse.ArgumentParser(description='Comments on lotsa stuff \
+    at the same time.')
+    parser.add_argument("-g",
+                        "--github-repo",
+                        dest="github_repo",
+                        required=True,
+                        help="The docker/repository within GitHub, for \
+                        example 'docker/escalation'")
+    parser.add_argument("-j",
+                        "--jira-repo",
+                        dest="jira_repo",
+                        required=True,
+                        help="The Jira repository name to link the GitHub \
+                        issues comments to, for example 'FIELD'")
+    parser.add_argument("-c",
+                        "--issue-count",
+                        dest="issue_count",
+                        help="Set the number of issues in the given Jira \
+                        repository. If none is given a value of 1000 will \
+                        be used.")
     parser.add_argument("--debug",
                         dest="debug",
                         action="store_true",
@@ -156,10 +81,17 @@ def main():
     hdlr = logging.StreamHandler(sys.stdout)
     hdlr.setFormatter(fmtr)
     logger.addHandler(hdlr)
+    # Validate given arguments
+    gh_repo = args.github_repo.split("/")
+    if not len(gh_repo[0]) > 0 and len(gh_repo[1]) > 0:
+        logger.error("Given GitHub repo: {0} is not a valid org/repository".format(gh_repo))
+        sys.exit(1)
+    if args.issue_count:
+        max_range = int(args.issue_count)
+    else:
+        max_range = 1000
+    generateComments(args.github_repo, args.jira_repo, max_range)
+    sys.exit(0)
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGTERM, signal_handler)
-    signal.signal(signal.SIGHUP, signal_handler)
-    signal.signal(signal.SIGQUIT, signal_handler)
-    signal.signal(signal.SIGINT, signal_handler)
     sys.exit(main())
